1910267573af24f387b40641215784024cee4085
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-06-22 14:24:37 -0700

    added NavLinks to Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <div>
+      <NavLink exact to ="/illustration">
+        Illustration
+      </NavLink>
+      <NavLink exact to ="/surface-design">
+        Surface Design
+      </NavLink>
+      <NavLink exact to ="/about">
+        About
+      </NavLink> <NavLink exact to ="/contact">
+        Contact
+      </NavLink>
+      <NavLink exact to ="/dev">
+        Development
+      </NavLink>
+    </div>
   )
 }
