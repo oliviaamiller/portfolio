2fcb5da011678ef0b7303a104a1379ffd43a1177
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-06-22 14:19:36 -0700

    created views and imported them into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
+import { Route, Switch } from 'react-router-dom';
+import Art from './views/Art';
+import Design from './views/Design';
+import About from './views/About';
+import Contact from './views/Contact';
+import Dev from './views/Dev';
+import Home from './views/Home';
+import Nav from './components/Nav';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/illustration">
+          <Art />
+        </Route>
+        <Route exact path="/surface-design">
+          <Design />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/dev">
+          <Dev />
+        </Route>
+      </Switch>
+    </>
+  );
 }
