17c25b7801b97bab8a06c9b1f641b5bb5548a5cc
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-07-06 10:38:40 -0700

    added padding to sides of nav link highlight

--- src/views/Design.jsx
+++ src/views/Design.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import styles from './Design.css';
+import rabbit from '../assets/rabbits4.png';
+import penguin from '../assets/penguins.jpeg';
+import hands from '../assets/hands3.jpg';
 
 export default function Design() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <section className={styles.designContainer}>
+        <div>
+          <img src={rabbit} alt="rabbits playing instruments and dancing" />
+        </div>
+        <div>
+          <img src={penguin} alt="penguins playing in the snow" />
+        </div>
+        <div>
+          <img src={hands} alt="hands forming shadow puppets" />
+        </div>
+        <div>img 4</div>
+      </section>
+    </>
+  );
 }
