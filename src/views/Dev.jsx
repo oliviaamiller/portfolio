6d78fc63845b58d93014d47069cbc6c3ad1fa1a4
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-07-06 18:38:01 -0700

    added website images to dev page

--- src/views/Dev.jsx
+++ src/views/Dev.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import styles from './Dev.css';
+import booty from '../assets/booty.png';
+import cooks from '../assets/cooksbooks.png';
+import duet from '../assets/duet.png';
+import sun from '../assets/sun.png';
 
 export default function Dev() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <section className={styles.devContainer}>
+        <div className={styles.booty}>
+          <img src={booty} />
+        </div>
+        <div className={styles.cooks}>
+          <img src={cooks} />
+        </div>
+        <div className={styles.duet}>
+          <img src={duet} />
+        </div>
+        <div className={styles.sun}>
+          <img src={sun} />
+        </div>
+      </section>
+    </>
+  );
 }
