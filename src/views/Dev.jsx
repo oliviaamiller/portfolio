8fe66e4e3d7c339e42f21788c43a79e3c6130676
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-08-09 15:34:39 -0700

    added links to dev images

--- src/views/Dev.jsx
+++ src/views/Dev.jsx
@@ -3,22 +3,31 @@ import booty from '../assets/booty.png';
 import cooks from '../assets/cooksbooks.png';
 import duet from '../assets/duet.png';
 import sun from '../assets/sun.png';
+import { Link } from 'react-router-dom';
 
 export default function Dev() {
   return (
     <>
       <section className={styles.devContainer}>
         <div className={styles.booty}>
-          <img src={booty} />
+          <Link to={`/dev/booty`}>
+            <img src={booty} />
+          </Link>
         </div>
         <div className={styles.cooks}>
-          <img src={cooks} />
+          <Link to={`/dev/cooks`}>
+            <img src={cooks} />
+          </Link>
         </div>
         <div className={styles.duet}>
-          <img src={duet} />
+          <Link to={`/dev/duet`}>
+            <img src={duet} />
+          </Link>
         </div>
         <div className={styles.sun}>
-          <img src={sun} />
+          <Link to={`/dev/sun`}>
+            <img src={sun} />
+          </Link>
         </div>
       </section>
     </>
