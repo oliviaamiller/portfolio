2c5f526b325d2474d2df90732ce41ad934349121
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-08-09 16:26:20 -0700

    added devData array

---
+++ src/services/DevData.js
@@ -0,0 +1,38 @@
+export const devData = [
+  {
+    id: 'booty',
+    src: require('../../assets/booty.png').default,
+    title: 'Booty or Death!',
+    about: 'about the app here....',
+    stack: 'tech stack here...',
+    git: 'git link here...',
+    deployed: 'deployed link here...',
+  },
+  {
+    id: 'cooks',
+    src: require('../../assets/cooksbooks.png').default,
+    title: 'Booty or Death!',
+    about: 'about the app here....',
+    stack: 'tech stack here...',
+    git: 'git link here...',
+    deployed: 'deployed link here...',
+  },
+  {
+    id: 'duet',
+    src: require('../../assets/duet.png').default,
+    title: 'Booty or Death!',
+    about: 'about the app here....',
+    stack: 'tech stack here...',
+    git: 'git link here...',
+    deployed: 'deployed link here...',
+  },
+  {
+    id: 'sun',
+    src: require('../../assets/sun.png').default,
+    title: 'Booty or Death!',
+    about: 'about the app here....',
+    stack: 'tech stack here...',
+    git: 'git link here...',
+    deployed: 'deployed link here...',
+  },
+];
